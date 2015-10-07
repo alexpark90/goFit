8f016212aedd8f016ec1b14235b51a8f68675b47
Alex Park <alexpark90@users.noreply.github.com>
2015-10-07 00:09:34 -0400

    Update MainGui.java

--- src/project/MainGui.java
+++ src/project/MainGui.java
@@ -24,7 +24,8 @@ import org.json.simple.parser.ParseException;
 
 /**
  *   Submitted by: Alex Yeji Park && Chris Sarvghadi
- *
+ *   Date: Apr. 15. 2015
+ * 
  *   Honor: I have completed this assignment on my own.
  *          In researching the assignment I got help/ideas from http://stackoverflow.com/ 
  *
